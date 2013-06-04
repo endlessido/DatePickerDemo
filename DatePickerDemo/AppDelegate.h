80c5967ad2279ce546bc17deaa90b25b11bf25ef
guihuan <guihuan@guihuantekiMacBook-Pro.local>
2013-06-04 17:16:02 +0800

    Initial Commit

---
+++ DatePickerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DatePickerDemo
+//
+//  Created by guihuan on 13-6-4.
+//  Copyright (c) 2013年 guihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
